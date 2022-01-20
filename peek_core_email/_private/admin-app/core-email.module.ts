4f6a4fc626f20ed6abcf2c214129620a62456a46
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-01-20 23:14:26 +1000

    PEEK-1381 Reformatted all code files with prettier
    The only alteration is 4 space indents instead of 2

--- peek_core_email/_private/admin-app/core-email.module.ts
+++ peek_core_email/_private/admin-app/core-email.module.ts
@@ -1,65 +1,63 @@
-import { CommonModule } from "@angular/common"
-import { FormsModule } from "@angular/forms"
-import { NgModule } from "@angular/core"
-import { CoreEmailAdminComponent } from "./core-email-admin.component"
-import { RouterModule, Routes } from "@angular/router"
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { CoreEmailAdminComponent } from "./core-email-admin.component";
+import { RouterModule, Routes } from "@angular/router";
 import {
     TupleActionPushNameService,
     TupleActionPushService,
     TupleDataObservableNameService,
-    TupleDataObserverService
-} from "@synerty/vortexjs"
+    TupleDataObserverService,
+} from "@synerty/vortexjs";
 
 import {
     coreEmailActionProcessorName,
     coreEmailFilt,
-    coreEmailObservableName
-} from "./PluginNames"
-import { AdminSettingListComponent } from "./setting-list/admin-setting-list.component"
+    coreEmailObservableName,
+} from "./PluginNames";
+import { AdminSettingListComponent } from "./setting-list/admin-setting-list.component";
 
 /**
  * Created by peek on 5/12/16.
  *
  */
 
-
 export const pluginRoutes: Routes = [
     {
         path: "",
-        component: CoreEmailAdminComponent
-    }
-
-]
+        component: CoreEmailAdminComponent,
+    },
+];
 
 export function tupleDataObservableNameServiceFactory() {
     return new TupleDataObservableNameService(
-        coreEmailObservableName, coreEmailFilt)
+        coreEmailObservableName,
+        coreEmailFilt
+    );
 }
 
 export function tupleActionPushNameServiceFactory() {
     return new TupleActionPushNameService(
-        coreEmailActionProcessorName, coreEmailFilt)
+        coreEmailActionProcessorName,
+        coreEmailFilt
+    );
 }
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        RouterModule.forChild(pluginRoutes)],
+    imports: [CommonModule, FormsModule, RouterModule.forChild(pluginRoutes)],
     exports: [],
     providers: [
-        TupleDataObserverService, {
+        TupleDataObserverService,
+        {
             provide: TupleDataObservableNameService,
-            useFactory: tupleDataObservableNameServiceFactory
-        }, TupleActionPushService, {
+            useFactory: tupleDataObservableNameServiceFactory,
+        },
+        TupleActionPushService,
+        {
             provide: TupleActionPushNameService,
-            useFactory: tupleActionPushNameServiceFactory
-        }
+            useFactory: tupleActionPushNameServiceFactory,
+        },
     ],
-    declarations: [CoreEmailAdminComponent,
-        AdminSettingListComponent,
-    ]
+    declarations: [CoreEmailAdminComponent, AdminSettingListComponent],
 })
-export class CoreEmailAdminModule {
-
-}
+export class CoreEmailAdminModule {}
--- peek_core_email/_private/admin-app/setting-list/admin-setting-list.component.ts
+++ peek_core_email/_private/admin-app/setting-list/admin-setting-list.component.ts
@@ -1,64 +1,72 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit } from "@angular/core";
 import {
     extend,
     NgLifeCycleEvents,
     Tuple,
     TupleLoader,
-    VortexService
-} from "@synerty/vortexjs"
-import { coreEmailFilt } from "../PluginNames"
-import { BalloonMsgService } from "@synerty/peek-plugin-base-js"
+    VortexService,
+} from "@synerty/vortexjs";
+import { coreEmailFilt } from "../PluginNames";
+import { BalloonMsgService } from "@synerty/peek-plugin-base-js";
 
 class SettingProperty extends Tuple {
-    id: number
-    settingId: number
-    key: string
-    type: string
-    
-    int_value: number
-    char_value: string
-    boolean_value: boolean
-    
+    id: number;
+    settingId: number;
+    key: string;
+    type: string;
+
+    int_value: number;
+    char_value: string;
+    boolean_value: boolean;
+
     constructor() {
-        super("c.s.p.setting.property")
+        super("c.s.p.setting.property");
     }
 }
 
 @Component({
     selector: "core-email-setting-list",
-    templateUrl: "./admin-setting-list.component.html"
+    templateUrl: "./admin-setting-list.component.html",
 })
-export class AdminSettingListComponent extends NgLifeCycleEvents implements OnInit {
-    items: SettingProperty[] = []
-    loader: TupleLoader
-    private readonly filt = extend({
-        key: "server.setting.data"
-    }, coreEmailFilt)
-    
+export class AdminSettingListComponent
+    extends NgLifeCycleEvents
+    implements OnInit
+{
+    items: SettingProperty[] = [];
+    loader: TupleLoader;
+    private readonly filt = extend(
+        {
+            key: "server.setting.data",
+        },
+        coreEmailFilt
+    );
+
     constructor(
         private balloonMsg: BalloonMsgService,
         vortexService: VortexService
     ) {
-        super()
-        
-        this.loader = vortexService.createTupleLoader(this, this.filt)
-        
+        super();
+
+        this.loader = vortexService.createTupleLoader(this, this.filt);
+
         this.loader.observable.subscribe(
-            tuples => this.items = <SettingProperty[]>tuples)
+            (tuples) => (this.items = <SettingProperty[]>tuples)
+        );
     }
-    
-    ngOnInit() {
-    }
-    
+
+    ngOnInit() {}
+
     saveClicked() {
-        this.loader.save()
+        this.loader
+            .save()
             .then(() => this.balloonMsg.showSuccess("Save Successful"))
-            .catch(e => this.balloonMsg.showError(e))
+            .catch((e) => this.balloonMsg.showError(e));
     }
-    
+
     resetClicked() {
-        this.loader.load()
+        this.loader
+            .load()
             .then(() => this.balloonMsg.showSuccess("Reset Successful"))
-            .catch(e => this.balloonMsg.showError(e))
+            .catch((e) => this.balloonMsg.showError(e));
     }
 }
